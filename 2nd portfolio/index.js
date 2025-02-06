67e0dfbcb83f8b3e31e5ebffb12bb33925fcdd0b
Dev_Mandani <devmandani2@gmail.com>
2025-02-06 17:13:24 +0530

    Add files via upload

---
+++ 2nd portfolio/index.js
@@ -0,0 +1,33 @@
+
+    setTimeout(() => {
+        let reload = document.getElementById("reloade");
+
+        reload.style.display="none";
+    }, 3000);
+
+
+
+    setTimeout(()=>{
+        let audio =document.getElementById(audio);
+
+        audio.style.display="none";
+    },3000);
+
+
+    function darkMode() {
+        let element = document.body;
+        let content = document.getElementById("lightModetext");
+        element.className = "dark-mode";
+        content.innerText = "Dark Mode is ON";
+    }
+    function lightMode() {
+        let element = document.body;
+        let content = document.getElementById("darkModetext");
+        element.className = "light-mode";
+        content.innerText = "Dark Mode is OFF";
+    }
+
+
+
+    
+
